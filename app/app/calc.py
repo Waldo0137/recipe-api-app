9b3b3cbe98a19f528ac66cad1d053471ee507d46
Wa-Vi.GO <geralnede@gmail.com>
2024-03-25 22:09:03 -0400

    Added psycopg2 and Configured Django

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x + y
+
+def sub(x,y):
+    """Substract x from y and return result."""
+    return y - x
